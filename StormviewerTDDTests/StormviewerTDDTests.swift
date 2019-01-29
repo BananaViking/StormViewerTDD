4ce670aecaabbd03ee9cde1f681dfcc480b0e4f4
Banana Viking <bananavikingcoder@gmail.com>
2019-01-29 12:36:50 -0600

    add testCellsHaveDisclosureIndicators()

--- StormviewerTDDTests/StormviewerTDDTests.swift
+++ StormviewerTDDTests/StormviewerTDDTests.swift
@@ -60,5 +60,21 @@ class StormviewerTDDTests: XCTestCase {
             XCTAssertEqual(cell.textLabel?.text, picture)
         }
     }
+    
+    func testCellsHaveDisclosureIndicators() {
+        // given
+        let sut = ViewController()
+        
+        // when
+        sut.loadViewIfNeeded()
+        
+        // then
+        for index in sut.pictures.indices {
+            let indexPath = IndexPath(item: index, section: 0)
+            let cell = sut.tableView(sut.tableView, cellForRowAt: indexPath)
+            
+            XCTAssertEqual(cell.accessoryType, .disclosureIndicator)
+        }
+    }
 
 }
